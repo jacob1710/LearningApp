cd9ecf7c757d5941c85a67b5bc1af95a15a88226
Jacob Scase <jacobscase@hotmail.com>
2021-07-04 16:05:11 +0100

    Add assets and models, parse json data and style data into content model

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -9,4 +9,48 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
+    
+    var styleData: Data?
+    
+    init() {
+        getLocalData()
+    }
+    
+    
+    func getLocalData(){
+        
+        
+        //Get a url to the json
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        //Read file into data object
+        do{
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            
+            //Try to decode the json into an array of modules
+            let jsonDecoder = JSONDecoder()
+            
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            
+            //Assign parsed modules to modules properly\
+            self.modules = modules
+        }catch{
+            print(error)
+        }
+       
+        
+        // Parse the style data
+        
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do {
+            let styleData = try Data(contentsOf: styleUrl!)
+            self.styleData = styleData
+        } catch {
+            print(error)
+        }
+        
+    }
+    
 }
